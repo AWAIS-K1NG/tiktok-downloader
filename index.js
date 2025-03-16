9d1148fb0178922c0c4d2f79293d4e71ed1e4886
AWAIS-K1NG <164879975+AWAIS-K1NG@users.noreply.github.com>
2025-03-17 02:08:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Telegram = require('node-telegram-bot-api');
 var request = require("request");
-var token = 'YOUR_TELEGRAM_BOT_TOKEN';
+var token = '6992969659:AAGeHdoYB3E3vJqVfH3MM_RCHTUCg5-BDok';
 
 // Configure the bot to use polling
 var opt = {
